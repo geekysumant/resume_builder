221c51674cb66a9deabed19d25bad022392c74c6
sumant <sumantk778@gmail.com>
2022-01-09 17:04:06 +0530

    cleanup

--- db/migrate/20220108175910_add_profile_to_projects.rb
+++
@@ -1,5 +0,0 @@
-class AddProfileToProjects < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :projects, :profile, null: false, foreign_key: true
-  end
-end
--- db/migrate/20220109054106_remove_profile_from_projects.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProfileFromProjects < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :projects, :profile, null: false, foreign_key: true
-  end
-end
