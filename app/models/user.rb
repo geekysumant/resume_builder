63a1a78a29863bede39642476e35da35108f72fb
sumant <sumantk778@gmail.com>
2022-01-09 22:13:48 +0530

    minor fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
     validates(:name, presence: true)
     validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
     validates(:password, presence: true, length: { minimum: 8 })
+    validates :password, confirmation: { case_sensitive: true }
 
     # adds password fields
     has_secure_password
